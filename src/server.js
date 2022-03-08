3fbe176d13dee5ee25bc7e1b2b1489a01eda0223
Andry Pebrianto <andrypeb227@gmail.com>
2022-03-08 09:47:22 +0700

    Konfigurasi package morgan, cors, dan helmet

--- src/server.js
+++ src/server.js
@@ -1,22 +1,44 @@
 const express = require("express");
 const morgan = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
 const cors = require("cors");
 const createError = require("http-errors");
+const chalk = require("chalk");
 require("dotenv").config();
 require("./config/db").connectToDB();
-const chalk = require("chalk");
 
 const app = express();
 
-// middlewares
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json({ limit: "2mb" }));
-app.use(express.urlencoded({ limit: "2mb", extended: true }));
-
 // env variables
+const NODE_ENV = process.env.NODE_ENV || "dev";
 const PORT = process.env.PORT || 5000;
 const PREFIX = process.env.PREFIX || "/api";
+
+// middlewares
+app.use(express.json({ limit: "2mb" }));
+app.use(express.urlencoded({ limit: "2mb", extended: true }));
+app.use(compression());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+// morgan & cors
+if (NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(
+    cors({
+      origin: ["http://localhost:3000", "https://localhost:3000"],
+      optionsSuccessStatus: 200, // For legacy browser support
+    })
+  );
+} else {
+  app.use(morgan("dev"));
+  app.use(
+    cors({ origin: ["http://localhost:3000", "https://localhost:3000"] })
+  );
+}
 
 // root route
 app.get("/", (req, res) => {
@@ -49,9 +71,7 @@ app.use((err, req, res, next) => {
 
 // running server
 app.listen(PORT, () => {
-  console.log(
-    `Server started on port ${PORT} with ${process.env.NODE_ENV} environment`
-  );
+  console.log(`Server started on port ${PORT} with ${NODE_ENV} environment`);
   console.log(chalk`Visit {rgb(128, 237, 153) http://localhost:${PORT}}`);
   console.log(chalk`Developed by {rgb(255, 92, 88) Andry Pebrianto}`);
 });
